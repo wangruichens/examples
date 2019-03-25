bdbe40cd297f375b61a39b446b9438c051acfae2
wangruichens <wangruichens@gmail.com>
2019-03-25 19:02:20 +0800

    tensorflow determine prime

--- forfun/tensorflow_isprime.py
+++ forfun/tensorflow_isprime.py
@@ -29,7 +29,7 @@ def primes():
         it=filter(_not_divisible(n),it)
 
 pos=[]
-lens=10000
+lens=100000
 for n in primes():
     if n<lens:
        pos.append(n)
@@ -57,4 +57,29 @@ while n <lens:
     n+=1
 
 
+from sklearn.model_selection import train_test_split
 
+train_x=np.expand_dims(np.array(train_x),axis=1)
+train_y=np.expand_dims(np.array(train_y),axis=1)
+
+x_train, x_test, y_train, y_test = train_test_split(train_x, train_y, test_size=0.1, random_state=42,shuffle=True)
+
+print(x_train.shape)
+print(x_test.shape)
+
+with tf.device('/cpu:0'):
+    model = tf.keras.Sequential([
+    tf.keras.layers.Dense(3, activation='relu'),
+    tf.keras.layers.Dense(2, activation='softmax')])
+
+
+# parallel_model = tf.keras.utils.multi_gpu_model(model, gpus=2)
+
+model.compile(optimizer=tf.train.AdamOptimizer(0.01),
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=10, batch_size=128)
+
+acc= model.evaluate(x_test, y_test, batch_size=128)
+print(acc)
