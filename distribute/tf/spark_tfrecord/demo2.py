70431aff94fd69c484934d87c31bc3bcc3c35b7b
wangruichens <wangruichens@gmail.com>
2019-05-09 13:17:56 +0800

    updates

--- distribute/tf/spark_tfrecord/demo2.py
+++ distribute/tf/spark_tfrecord/demo2.py
@@ -1,39 +1,57 @@
-# df.repartition(1).write.format("tfrecords").option("recordType", "Example").save(path)
-# ss = SparkSession.builder \
-#     .appName("train") \
-#     .enableHiveSupport() \
-#     .getOrCreate()
-# df=ss.sql('select * from mlg.g_tfrecord_test')
-
-path='hdfs://cluster/user/wangrc/test1.tfrecord/part-r-0000'
-filenames=[]
-for i in range(10):
-    filenames.append(path+str(i))
-
-print(filenames)
+# export HADOOP_HOME=/tmp/wangrc/hadoop-2.7.3
+# export CLASSPATH=$(${HADOOP_HOME}/bin/hadoop classpath --glob)
 
 import tensorflow as tf
+from pyspark.sql import SparkSession
 tf.enable_eager_execution()
 
+####################load dataframe#########################
+ss = SparkSession.builder \
+    .appName("train") \
+    .enableHiveSupport() \
+    .getOrCreate()
+df=ss.sql('select * from mlg.g_tfrecord_test')
 
-path='hdfs://cluster/user/wangrc/test1.tfrecord/part-r-00000'
+
+####################save dataframe to tfrecord #########################
+folder='test.tfrecord'
+path='hdfs://cluster/user/wangrc/test.tfrecord/part-r-00000'
+df.repartition(1).write.format("tfrecords").mode("overwrite").option("recordType", "Example").save(folder)
+
+
+####################load tfrecord to dataframe #########################
+df = ss.read.format("tfrecords").option("recordType", "Example").load(path)
+
+
+
+####################read tfrecord DEPRECATED #########################
+record_iterator = tf.python_io.tf_record_iterator(path=path)
+for string_record in record_iterator:
+    example = tf.train.Example()
+    example.ParseFromString(string_record)
+    print(example)
+
+
+####################read tfrecord #########################
 raw_dataset = tf.data.TFRecordDataset(path)
 for raw_record in raw_dataset.take(2):
     print(repr(raw_record))
 
 
-feature_description = {
-    'id': tf.FixedLenFeature([], tf.int64, default_value=0),
-    'IntegerCol': tf.FixedLenFeature([], tf.int64, default_value=0),
-    'LongCol': tf.FixedLenFeature([], tf.int64, default_value=0),
-    'FloatCol': tf.FixedLenFeature([], tf.float32, default_value=0.0),
-    'DoubleCol': tf.FixedLenFeature([], tf.float32, default_value=0.0),
-    'VectorCol': tf.FixedLenFeature((2), tf.float32, default_value=[0,0]),
-    'StringCol': tf.FixedLenFeature([], tf.string, default_value=''),
-    }
+####################decode tfrecord #########################
+
 
 def _parse_function(example_proto):
-  return tf.parse_single_example(example_proto, feature_description)
+    feature_description = {
+        'id': tf.FixedLenFeature([], tf.int64, default_value=0),
+        'IntegerCol': tf.FixedLenFeature([], tf.int64, default_value=0),
+        'LongCol': tf.FixedLenFeature([], tf.int64, default_value=0),
+        'FloatCol': tf.FixedLenFeature([], tf.float32, default_value=0.0),
+        'DoubleCol': tf.FixedLenFeature([], tf.float32, default_value=0.0),
+        'VectorCol': tf.FixedLenFeature((2), tf.float32, default_value=[0, 0]),
+        'StringCol': tf.FixedLenFeature([], tf.string, default_value=''),
+    }
+    return tf.parse_single_example(example_proto, feature_description)
 
 dataset = raw_dataset.map(_parse_function)
 
@@ -41,9 +59,3 @@ for p in dataset.take(2):
     print(repr(p))
 
 
-
-record_iterator = tf.python_io.tf_record_iterator(path='hdfs://cluster/user/wangrc/tfrecord_test/part-r-00000')
-for string_record in record_iterator:
-    example = tf.train.Example()
-    example.ParseFromString(string_record)
-    print(example)
