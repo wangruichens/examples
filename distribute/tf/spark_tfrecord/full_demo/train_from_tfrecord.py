2db5b217e6b3340ffaebf294610c416c04afa4cd
wangruichens <wangruichens@gmail.com>
2019-06-10 15:43:55 +0800

    deepfm complete

--- distribute/tf/spark_tfrecord/full_demo/train_from_tfrecord.py
+++ distribute/tf/spark_tfrecord/full_demo/train_from_tfrecord.py
@@ -9,10 +9,10 @@ import tensorflow as tf
 from tensorflow.python.client import device_lib
 from tensorflow import keras
 
+
 def get_available_gpus():
     local_device_protos = device_lib.list_local_devices()
     return [x.name for x in local_device_protos if x.device_type == 'GPU']
-
 
 
 def parse_args():
@@ -33,7 +33,7 @@ def df_to_hive(spark, df, table_name):
 
 def main(args):
     print(get_available_gpus())
-    gpu_num=len(get_available_gpus())
+    gpu_num = len(get_available_gpus())
 
     path = 'hdfs://cluster/user/wangrc/mnist.tfrecord/train/part-r-0000'
     filenames = []
@@ -44,21 +44,22 @@ def main(args):
     feature_description = {
         'label': tf.FixedLenFeature((), tf.int64),
         'features': tf.FixedLenFeature((784), tf.int64),
-        }
+    }
 
-    #Note here:
+    # Note here:
     #  tf.enable_eager_execution() fetch data when you call it.
     #  So the data shape can not be inferred. Need to specific the input data shape with (-1,28,28,1) and the label (-1,1)
     def _parse_function(example_proto):
-        parsed_feature= tf.parse_single_example(example_proto, feature_description)
-        return tf.reshape(tf.cast(parsed_feature['features'], tf.float32),[28,28,1]), \
+        parsed_feature = tf.parse_single_example(example_proto, feature_description)
+        return tf.reshape(tf.cast(parsed_feature['features'], tf.float32), [28, 28, 1]), \
                tf.cast(parsed_feature['label'], tf.float32)
 
-    dataset = raw_dataset.map(_parse_function,num_parallel_calls=4).shuffle(buffer_size=256).prefetch(256).batch(256).repeat()
+    dataset = raw_dataset.map(_parse_function, num_parallel_calls=4).shuffle(buffer_size=256).prefetch(256).batch(
+        256).repeat()
     # for p in dataset.take(1):
     #     print(repr(p))
 
-    inputs = keras.layers.Input(shape=(28,28,1))
+    inputs = keras.layers.Input(shape=(28, 28, 1))
     x = keras.layers.Conv2D(64, (3, 3), activation='relu')(inputs)
     x = keras.layers.MaxPooling2D(pool_size=(2, 2))(x)
     x = keras.layers.Dropout(0.5)(x)
@@ -68,7 +69,7 @@ def main(args):
     predictions = keras.layers.Dense(10, activation='softmax')(x)
     model = keras.Model(inputs=inputs, outputs=predictions)
 
-    if gpu_num>1:
+    if gpu_num > 1:
         model = keras.utils.multi_gpu_model(model, gpus=gpu_num)
     model.compile(optimizer=tf.train.AdamOptimizer(0.001),
                   loss='sparse_categorical_crossentropy',
@@ -86,11 +87,10 @@ def main(args):
         inputs={'input_image': model.input},
         outputs={t.name: t for t in model.outputs})
 
-    print('\nmodel saved')    
+    print('\nmodel saved')
     keras.backend.clear_session()
-    
+
 
 if __name__ == '__main__':
-
     args = parse_args()
     main(args)
