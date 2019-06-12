d13e8f6dcea18b03e51f303ed6073da1604b434d
wangruichens <wangruichens@gmail.com>
2019-06-12 10:27:19 +0800

    tf estimator serving

--- recsys_models/deepfm/deepfm.py
+++ recsys_models/deepfm/deepfm.py
@@ -12,7 +12,7 @@ tf.app.flags.DEFINE_integer("embedding_size", 32, "Embedding size")
 tf.app.flags.DEFINE_float("learning_rate", 0.001, "Learning rate")
 tf.app.flags.DEFINE_float("dropout", 0.5, "Dropout rate")
 tf.app.flags.DEFINE_string("task_type", 'train', "Task type {train, infer, eval, export}")
-tf.app.flags.DEFINE_integer("num_epochs", 1, "Number of epochs")
+tf.app.flags.DEFINE_integer("num_epochs", 10, "Number of epochs")
 tf.app.flags.DEFINE_integer("batch_size", 256, "Number of batch size")
 tf.app.flags.DEFINE_string("deep_layers", '200,200,200', "deep layers")
 tf.app.flags.DEFINE_string("dataset_path", '/home/wangrc/Downloads/deepfmdata/', "Data path")
@@ -743,8 +743,64 @@ def main(argv):
     elif FLAGS.task_type == 'eval':
         deepfm.evaluate(input_fn=lambda: input_fn(eval_files, num_epochs=1, batch_size=FLAGS.batch_size))
 
-    feature_description.pop('label')
-    serving_fn = tf.estimator.export.build_parsing_serving_input_receiver_fn(feature_description)
+    # feature_description.pop('label')
+    # serving_fn = tf.estimator.export.build_parsing_serving_input_receiver_fn(feature_description)
+
+    features = {
+        "u_id": tf.placeholder(dtype=tf.int32, shape=(None, 1), name='u_id'),
+        "i_id": tf.placeholder(dtype=tf.int32, shape=(None, 1), name='i_id'),
+
+        "i_channel": tf.placeholder(dtype=tf.string, shape=(None, 1), name='i_channel'),
+
+        "u_brand": tf.placeholder(dtype=tf.string, shape=(None, 1), name='u_brand'),
+        "u_operator": tf.placeholder(dtype=tf.string, shape=(None, 1), name='u_operator'),
+        "u_activelevel": tf.placeholder(dtype=tf.string, shape=(None, 1), name='u_activelevel'),
+
+        "u_age": tf.placeholder(dtype=tf.string, shape=(None, 1), name='u_age'),
+        "u_marriage": tf.placeholder(dtype=tf.string, shape=(None, 1), name='u_marriage'),
+        "u_sex": tf.placeholder(dtype=tf.string, shape=(None, 1), name='u_sex'),
+        "u_sex_age": tf.placeholder(dtype=tf.string, shape=(None, 1), name='u_sex_age'),
+        "u_sex_marriage": tf.placeholder(dtype=tf.string, shape=(None, 1), name='u_sex_marriage'),
+        "u_age_marriage": tf.placeholder(dtype=tf.string, shape=(None, 1), name='u_age_marriage'),
+
+        "i_hot_news": tf.placeholder(dtype=tf.string, shape=(None, 1), name='i_hot_news'),
+        "i_is_recommend": tf.placeholder(dtype=tf.string, shape=(None, 1), name='i_is_recommend'),
+
+        "i_info_exposed_amt": tf.placeholder(dtype=tf.float32, shape=(None, 1), name='i_info_exposed_amt'),
+        "i_info_clicked_amt": tf.placeholder(dtype=tf.float32, shape=(None, 1), name='i_info_clicked_amt'),
+        "i_info_ctr": tf.placeholder(dtype=tf.float32, shape=(None, 1), name='i_info_ctr'),
+
+        "i_cate_exposed_amt": tf.placeholder(dtype=tf.float32, shape=(None, 1), name='i_cate_exposed_amt'),
+        "i_cate_clicked_amt": tf.placeholder(dtype=tf.float32, shape=(None, 1), name='i_cate_clicked_amt'),
+        "i_category_ctr": tf.placeholder(dtype=tf.float32, shape=(None, 1), name='i_category_ctr'),
+
+        "c_uid_type_ctr_1": tf.placeholder(dtype=tf.float32, shape=(None, 1), name='c_uid_type_ctr_1'),
+        "c_uid_type_clicked_amt_1": tf.placeholder(dtype=tf.float32, shape=(None, 1), name='c_uid_type_clicked_amt_1'),
+        "c_uid_type_exposed_amt_1": tf.placeholder(dtype=tf.float32, shape=(None, 1), name='c_uid_type_exposed_amt_1'),
+        "c_uid_type_ctr_3": tf.placeholder(dtype=tf.float32, shape=(None, 1), name='c_uid_type_ctr_3'),
+        "c_uid_type_clicked_amt_3": tf.placeholder(dtype=tf.float32, shape=(None, 1), name='c_uid_type_clicked_amt_3'),
+        "c_uid_type_exposed_amt_3": tf.placeholder(dtype=tf.float32, shape=(None, 1), name='c_uid_type_exposed_amt_3'),
+        "c_uid_type_ctr_7": tf.placeholder(dtype=tf.float32, shape=(None, 1), name='c_uid_type_ctr_7'),
+        "c_uid_type_clicked_amt_7": tf.placeholder(dtype=tf.float32, shape=(None, 1), name='c_uid_type_clicked_amt_7'),
+        "c_uid_type_exposed_amt_7": tf.placeholder(dtype=tf.float32, shape=(None, 1), name='c_uid_type_exposed_amt_7'),
+        "c_uid_type_ctr_14": tf.placeholder(dtype=tf.float32, shape=(None, 1), name='c_uid_type_ctr_14'),
+        "c_uid_type_clicked_amt_14": tf.placeholder(dtype=tf.float32, shape=(None, 1),
+                                                    name='c_uid_type_clicked_amt_14'),
+        "c_uid_type_exposed_amt_14": tf.placeholder(dtype=tf.float32, shape=(None, 1),
+                                                    name='c_uid_type_exposed_amt_14'),
+
+        "c_user_flavor": tf.placeholder(dtype=tf.float32, shape=(None, 1), name='c_user_flavor'),
+
+        "u_activetime_at1": tf.placeholder(dtype=tf.float32, shape=(None, 1), name='u_activetime_at1'),
+        "u_activetime_at2": tf.placeholder(dtype=tf.float32, shape=(None, 1), name='u_activetime_at2'),
+        "u_activetime_at3": tf.placeholder(dtype=tf.float32, shape=(None, 1), name='u_activetime_at3'),
+        "u_activetime_at4": tf.placeholder(dtype=tf.float32, shape=(None, 1), name='u_activetime_at4'),
+        "u_activetime_at5": tf.placeholder(dtype=tf.float32, shape=(None, 1), name='u_activetime_at5'),
+
+        "i_mini_img_size": tf.placeholder(dtype=tf.float32, shape=(None, 1), name='i_mini_img_size'),
+        "i_comment_count": tf.placeholder(dtype=tf.float32, shape=(None, 1), name='i_comment_count'),
+    }
+    serving_fn = tf.estimator.export.build_raw_serving_input_receiver_fn(features)
 
     deepfm.export_savedmodel(
         export_dir_base=FLAGS.export_path,
--- recsys_models/deepfm/load_tfrecords.py
+++ recsys_models/deepfm/load_tfrecords.py
@@ -1,60 +1,136 @@
+import json
+import requests
 import tensorflow as tf
 
 tf.enable_eager_execution()
-feature_description = {
-    "u_id": tf.FixedLenFeature(dtype=tf.int64, shape=1),
-    "i_id": tf.FixedLenFeature(dtype=tf.int64, shape=1),
-    "i_channel": tf.FixedLenFeature(dtype=tf.string, shape=1),
-    "u_brand": tf.FixedLenFeature(dtype=tf.string, shape=1),
-    "u_operator": tf.FixedLenFeature(dtype=tf.string, shape=1),
-    "u_activelevel": tf.FixedLenFeature(dtype=tf.string, shape=1),
-    "u_age": tf.FixedLenFeature(dtype=tf.string, shape=1),
-    "u_marriage": tf.FixedLenFeature(dtype=tf.string, shape=1),
-    "u_sex": tf.FixedLenFeature(dtype=tf.string, shape=1, ),
-    "u_sex_age": tf.FixedLenFeature(dtype=tf.string, shape=1),
-    "u_sex_marriage": tf.FixedLenFeature(dtype=tf.string, shape=1),
-    "u_age_marriage": tf.FixedLenFeature(dtype=tf.string, shape=1),
-    "i_hot_news": tf.FixedLenFeature(dtype=tf.string, shape=1),
-    "i_is_recommend": tf.FixedLenFeature(dtype=tf.string, shape=1),
-    "i_info_exposed_amt": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "i_info_clicked_amt": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "i_info_ctr": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "i_cate_exposed_amt": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "i_cate_clicked_amt": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "i_category_ctr": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "c_uid_type_ctr_1": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "c_uid_type_clicked_amt_1": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "c_uid_type_exposed_amt_1": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "c_uid_type_ctr_3": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "c_uid_type_clicked_amt_3": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "c_uid_type_exposed_amt_3": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "c_uid_type_ctr_7": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "c_uid_type_clicked_amt_7": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "c_uid_type_exposed_amt_7": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "c_uid_type_ctr_14": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "c_uid_type_clicked_amt_14": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "c_uid_type_exposed_amt_14": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "c_user_flavor": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "u_activetime_at1": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "u_activetime_at2": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "u_activetime_at3": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "u_activetime_at4": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "u_activetime_at5": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "i_mini_img_size": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-    "i_comment_count": tf.FixedLenFeature(dtype=tf.float32, shape=1),
-}
-categoryFeatureNa = '####'
 
 
-def _parse_example(serial_exmp):
-    features = tf.parse_single_example(serial_exmp, features=feature_description)
-    return features
+record_iterator = tf.python_io.tf_record_iterator(path='part-r-00000')
+for string_record in record_iterator:
+    example = tf.train.Example()
+    example.ParseFromString(string_record)
+    # print(example)
+    break
+
+p = {"c_uid_type_clicked_amt_1": 107.0,
+     "c_uid_type_clicked_amt_14": 517.0,
+     "c_uid_type_clicked_amt_3": 517.0,
+     "c_uid_type_clicked_amt_7": 517.0,
+     "c_uid_type_ctr_1": 0.53233802318573,
+     "c_uid_type_ctr_14": 0.5340909957885742,
+     "c_uid_type_ctr_3": 0.5340909957885742,
+     "c_uid_type_ctr_7": 0.5340909957885742,
+     "c_uid_type_exposed_amt_1": 201.0,
+     "c_uid_type_exposed_amt_14": 968.0,
+     "c_uid_type_exposed_amt_3": 968.0,
+     "c_uid_type_exposed_amt_7": 968.0,
+
+     "c_user_flavor": 1.0312000513076782,
+
+     "i_cate_clicked_amt": 490384.0
+    ,
+
+     "i_cate_exposed_amt": 3666584.0
+    ,
+
+     "i_category_ctr": 0.1337440013885498
+    ,
+
+     "i_channel": "xinwen",
+
+     "i_comment_count": 0.0
+    ,
+
+     "i_hot_news": "####",
+
+     "i_id": 459924
+    ,
+
+     "i_info_clicked_amt"
+     : 6.0
+    ,
+
+     "i_info_ctr"
+     : 0.125
+    ,
+
+     "i_info_exposed_amt"
+     : 48.0
+    ,
+
+     "i_is_recommend": "####"
+    ,
+
+     "i_mini_img_size"
+     : 0.0
+    ,
+
+     "u_activelevel": "####"
+    ,
+
+     "u_activetime_at1"
+     : 0.0
+    ,
+
+     "u_activetime_at2"
+     : 0.0
+    ,
+
+     "u_activetime_at3"
+     : 0.0
+    ,
+
+     "u_activetime_at4"
+     : 0.0
+    ,
+
+     "u_activetime_at5"
+     : 0.0
+    ,
+
+     "u_age": "####"
+    ,
+
+     "u_age_marriage": "####"
+    ,
+
+     "u_brand": "####"
+    ,
+
+     "u_id": 1044329
+    ,
+
+     "u_marriage": "####"
+    ,
+
+     "u_operator": "####"
+    ,
+
+     "u_sex": "####"
+    ,
+
+     "u_sex_age": "####"
+    ,
+
+     "u_sex_marriage": "####"
+     }
 
 
-datadir = ['part-r-00000']
-dataset = tf.data.TFRecordDataset(datadir)
-# dataset = dataset.map(_parse_example, num_parallel_calls=8)
-# for i in dataset.take(1):
-#     print(i)
-#
 # saved_model_cli run --dir /home/wangrc/Downloads/export/1560231853/ --tag_set serve --signature_def serving_default --input_exprs='examples=[b"\n\x8d\t\n\x16\n\ti_channel\x12\t\n\x07\n\x05meinv\n\x1e\n\x12i_info_exposed_amt\x12\x08\x12\x06\n\x04\x00\x00@B\n\x1e\n\x12i_info_clicked_amt\x12\x08\x12\x06\n\x04\x00\x00\xc0@\n\x16\n\ni_info_ctr\x12\x08\x12\x06\n\x04\x00\x00\x00>\n\x1e\n\x12i_cate_exposed_amt\x12\x08\x12\x06\n\x04`\xca_J\n\x1e\n\x12i_cate_clicked_amt\x12\x08\x12\x06\n\x04\x00r\xefH\n\x1a\n\x0ei_category_ctr\x12\x08\x12\x06\n\x040\xf4\x08>\n\x1c\n\x10c_uid_type_ctr_1\x12\x08\x12\x06\n\x04NG\x08?\n$\n\x18c_uid_type_clicked_amt_1\x12\x08\x12\x06\n\x04\x00\x00\xd6B\n$\n\x18c_uid_type_exposed_amt_1\x12\x08\x12\x06\n\x04\x00\x00IC\n\x1c\n\x10c_uid_type_ctr_3\x12\x08\x12\x06\n\x040\xba\x08?\n$\n\x18c_uid_type_clicked_amt_3\x12\x08\x12\x06\n\x04\x00@\x01D\n$\n\x18c_uid_type_exposed_amt_3\x12\x08\x12\x06\n\x04\x00\x00rD\n\x1c\n\x10c_uid_type_ctr_7\x12\x08\x12\x06\n\x040\xba\x08?\n$\n\x18c_uid_type_clicked_amt_7\x12\x08\x12\x06\n\x04\x00@\x01D\n$\n\x18c_uid_type_exposed_amt_7\x12\x08\x12\x06\n\x04\x00\x00rD\n\x1d\n\x11c_uid_type_ctr_14\x12\x08\x12\x06\n\x040\xba\x08?\n%\n\x19c_uid_type_clicked_amt_14\x12\x08\x12\x06\n\x04\x00@\x01D\n%\n\x19c_uid_type_exposed_amt_14\x12\x08\x12\x06\n\x04\x00\x00rD\n\x19\n\rc_user_flavor\x12\x08\x12\x06\n\x04]\xfe\x83?\n\x16\n\nu_operator\x12\x08\n\x06\n\x04####\n\x1c\n\x10u_activetime_at1\x12\x08\x12\x06\n\x04\x00\x00\x00\x00\n\x1c\n\x10u_activetime_at2\x12\x08\x12\x06\n\x04\x00\x00\x00\x00\n\x1c\n\x10u_activetime_at3\x12\x08\x12\x06\n\x04\x00\x00\x00\x00\n\x1c\n\x10u_activetime_at4\x12\x08\x12\x06\n\x04\x00\x00\x00\x00\n\x1c\n\x10u_activetime_at5\x12\x08\x12\x06\n\x04\x00\x00\x00\x00\n\x1b\n\x0fi_mini_img_size\x12\x08\x12\x06\n\x04\x00\x00\x00\x00\n\x1b\n\x0fi_comment_count\x12\x08\x12\x06\n\x04\x00\x00\x00\x00\n\x13\n\x07u_brand\x12\x08\n\x06\n\x04####\n\x19\n\ru_activelevel\x12\x08\n\x06\n\x04####\n\x11\n\x05u_age\x12\x08\n\x06\n\x04####\n\x16\n\nu_marriage\x12\x08\n\x06\n\x04####\n\x11\n\x05u_sex\x12\x08\n\x06\n\x04####\n\x15\n\tu_sex_age\x12\x08\n\x06\n\x04####\n\x1a\n\x0eu_sex_marriage\x12\x08\n\x06\n\x04####\n\x1a\n\x0eu_age_marriage\x12\x08\n\x06\n\x04####\n\x16\n\ni_hot_news\x12\x08\n\x06\n\x04####\n\x1a\n\x0ei_is_recommend\x12\x08\n\x06\n\x04####\n\x0f\n\x04u_id\x12\x07\x1a\x05\n\x03\xe9\xde?\n\x0f\n\x04i_id\x12\x07\x1a\x05\n\x03\x94\x89\x1c"]'
+
+# tensorflow_model_server \
+#   --rest_api_port=8501 \
+#   --model_name=deepfm \
+#   --model_base_path="/home/wangrc/Desktop/"
+
+data = json.dumps({"signature_name": "predict", "instances": [p]})
+# print(data)
+
+headers = {"content-type": "application/json"}
+json_response = requests.post('http://localhost:8501/v1/models/deepfm:predict', data=data, headers=headers)
+# print(json_response.text)
+predictions = json.loads(json_response.text)['predictions']
+print(predictions)
+
+# curl -d '{"instances": [{"i_comment_count": [0.13]}]}' -X POST http://localhost:8501/v1/models/deepfm:predict
+
